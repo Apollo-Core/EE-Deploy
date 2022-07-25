bbb8cf912b7ecf5e16a22e674701028ac1b61d08
Stefan Pedratscher <Stefan.Pedratscher@student.uibk.ac.at>
2022-07-25 14:31:04 +0200

    updated apollo lambda test

--- src/test/java/at/uibk/dps/ee/deploy/lambda/ApolloLambdaTest.java
+++ src/test/java/at/uibk/dps/ee/deploy/lambda/ApolloLambdaTest.java
@@ -4,6 +4,7 @@ import at.uibk.dps.ee.deploy.resources.ReadTestStrings;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -23,15 +24,34 @@ public class ApolloLambdaTest {
         ApolloLambda apolloLambda = spy(ApolloLambda.class);
 
         JsonObject json = new JsonObject();
-        json.addProperty("config", ReadTestStrings.configString);
-        json.addProperty("spec", ReadTestStrings.specString);
-        json.addProperty("input", ReadTestStrings.inputString);
+        json.addProperty("configuration", ReadTestStrings.configString);
+        json.addProperty("specification", ReadTestStrings.specString);
+
+        JsonObject obj = JsonParser.parseString(ReadTestStrings.inputString).getAsJsonObject();
+        obj.keySet().forEach((key) -> json.addProperty(key, obj.get(key).getAsNumber()));
+
         APIGatewayProxyRequestEvent input = new APIGatewayProxyRequestEvent().withBody(json.toString());
 
-        Mockito.doReturn(new JsonObject()).when(apolloLambda).run(ReadTestStrings.inputString, ReadTestStrings.specString, ReadTestStrings.configString);
+        Mockito.doReturn(new JsonObject()).when(apolloLambda).run(obj.toString(), ReadTestStrings.specString, ReadTestStrings.configString);
         APIGatewayProxyResponseEvent response = apolloLambda.handleRequest(input, null);
 
         assertEquals(Integer.valueOf(200), response.getStatusCode());
         assertEquals(response.getBody(), new JsonObject().toString());
+    }
+
+    /**
+     * Test missing configuration.
+     */
+    @Test
+    public void testMissingConfiguration() {
+        ApolloLambda apolloLambda = spy(ApolloLambda.class);
+        JsonObject json = new JsonObject();
+        APIGatewayProxyRequestEvent input = new APIGatewayProxyRequestEvent().withBody(json.toString());
+
+        Mockito.doReturn(new JsonObject()).when(apolloLambda).run(json.toString(), ReadTestStrings.specString, ReadTestStrings.configString);
+        APIGatewayProxyResponseEvent response = apolloLambda.handleRequest(input, null);
+
+        assertEquals(Integer.valueOf(422), response.getStatusCode());
+        assertEquals("Missing configuration or specification.", response.getBody());
     }
 }
